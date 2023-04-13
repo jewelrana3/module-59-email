8a0198d0efce779c5ed00bf42a15e8fa139422d0
jewelrana3 <juyelrana7752@gmail.com>
2023-04-13 23:34:35 +0600

    form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email,setEmail] = useState('')
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        console.log(event.target.email.value)
+    }
+
+    const handleEmail = (event)=>{
+        // console.log(event.target.value);
+        setEmail(event.target.value)
+    }
+
+    const passwordBlur=(event)=>{
+        // console.log(event.target.value)
+    }
     return (
         <div>
-            <h2>register mama</h2>
+            <h2>Please Register</h2>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleEmail} type="email" name="email" id="email" placeholder='Yoour Email'/><br />
+                <input onBlur={passwordBlur} type="password" name="password" id="password" placeholder='Your Password'/><br />
+                <input type="submit" value="register" />
+            </form>
         </div>
     );
 };
